0cdc1d1cbafc64a510d8cdaca1031653abd2e98c
Dan Kim <dan@basecamp.com>
2016-02-04 15:45:11 -0600

    Fix missing asset. Fix gitignore.
    Fix turbolinks_bridge native calls that were lost with gitignore issue

---
+++ turbolinks/src/main/assets/js/turbolinks_bridge.js
@@ -0,0 +1,109 @@
+function TLWebView(controller) {
+    this.controller = controller
+    controller.adapter = this
+
+    var turbolinksIsReady = typeof Turbolinks !== "undefined" && Turbolinks !== null
+    TurbolinksNative.setTurbolinksIsReady(turbolinksIsReady);
+    TurbolinksNative.setFirstRestorationIdentifier(this.controller.restorationIdentifier);
+}
+
+TLWebView.prototype = {
+    // -----------------------------------------------------------------------
+    // Starting point
+    // -----------------------------------------------------------------------
+
+    visitLocationWithActionAndRestorationIdentifier: function(location, action, restorationIdentifier) {
+        this.controller.startVisitToLocationWithAction(location, action, restorationIdentifier)
+    },
+
+    // -----------------------------------------------------------------------
+    // Current visit
+    // -----------------------------------------------------------------------
+
+    issueRequestForVisitWithIdentifier: function(identifier) {
+        if (identifier == this.currentVisit.identifier) {
+            this.currentVisit.issueRequest()
+        }
+    },
+
+    changeHistoryForVisitWithIdentifier: function(identifier) {
+        if (identifier == this.currentVisit.identifier) {
+            this.currentVisit.changeHistory()
+        }
+    },
+
+    loadCachedSnapshotForVisitWithIdentifier: function(identifier) {
+        if (identifier == this.currentVisit.identifier) {
+            this.currentVisit.loadCachedSnapshot()
+        }
+    },
+
+    loadResponseForVisitWithIdentifier: function(identifier) {
+        if (identifier == this.currentVisit.identifier) {
+            this.currentVisit.loadResponse()
+        }
+    },
+
+    cancelVisitWithIdentifier: function(identifier) {
+        if (identifier == this.currentVisit.identifier) {
+            this.currentVisit.cancel()
+        }
+    },
+
+    // -----------------------------------------------------------------------
+    // Adapter
+    // -----------------------------------------------------------------------
+
+    visitProposedToLocationWithAction: function(location, action) {
+        TurbolinksNative.visitProposedToLocationWithAction(location.absoluteURL, action);
+    },
+
+    visitStarted: function(visit) {
+        this.currentVisit = visit
+        TurbolinksNative.visitStarted(visit.identifier, visit.hasCachedSnapshot());
+    },
+
+    visitRequestStarted: function(visit) {
+    },
+
+    visitRequestCompleted: function(visit) {
+        TurbolinksNative.visitRequestCompleted(visit.identifier);
+    },
+
+    visitRequestFailedWithStatusCode: function(visit, statusCode) {
+        TurbolinksNative.visitRequestFailedWithStatusCode(visit.identifier, statusCode);
+    },
+
+    visitRequestFinished: function(visit) {
+    },
+
+    visitRendered: function(visit) {
+        this.afterNextRepaint(function() {
+            TurbolinksNative.visitRendered(visit.identifier)
+        })
+    },
+
+    visitCompleted: function(visit) {
+        TurbolinksNative.visitCompleted(visit.identifier, visit.restorationIdentifier)
+    },
+
+    pageInvalidated: function() {
+        TurbolinksNative.pageInvalidated()
+    },
+
+    // -----------------------------------------------------------------------
+    // Private
+    // -----------------------------------------------------------------------
+
+    afterNextRepaint: function(callback) {
+      requestAnimationFrame(function() {
+        requestAnimationFrame(callback)
+      })
+    }
+}
+
+try {
+    window.webView = new TLWebView(Turbolinks.controller)
+} catch (e) { // Most likely reached a page where Turbolinks.controller returned "Uncaught ReferenceError: Turbolinks is not defined"
+    TurbolinksNative.turbolinksDoesNotExist()
+}
--- turbolinks/src/main/java/com/basecamp/turbolinks/TurbolinksTestActivity.java
+++ turbolinks/src/main/java/com/basecamp/turbolinks/TurbolinksTestActivity.java
@@ -5,34 +5,4 @@ import android.app.Activity;
 /**
  * For testing and demonstration purposes only.
  */
-public class TurbolinksTestActivity extends Activity implements TurbolinksAdapter {
-    @Override
-    public void onPageFinished() {
-
-    }
-
-    @Override
-    public void onReceivedError(int errorCode) {
-
-    }
-
-    @Override
-    public void pageInvalidated() {
-
-    }
-
-    @Override
-    public void requestFailedWithStatusCode(int statusCode) {
-
-    }
-
-    @Override
-    public void visitCompleted() {
-
-    }
-
-    @Override
-    public void visitProposedToLocationWithAction(String location, String action) {
-
-    }
-}
+public class TurbolinksTestActivity extends Activity {}
