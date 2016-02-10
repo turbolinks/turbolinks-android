d49ffc99bab762b33bed38bd86e8b9d126c16a8a
Dan Kim <dan@basecamp.com>
2016-02-10 09:17:00 -0600

    Fix TestBuildConfig to be dynamic

--- turbolinks/src/test/java/com/basecamp/turbolinks/TestBuildConfig.java
+++ turbolinks/src/test/java/com/basecamp/turbolinks/TestBuildConfig.java
@@ -1,12 +1,12 @@
 package com.basecamp.turbolinks;
 
+import com.basecamp.turbolinks.BuildConfig;
+
 public final class TestBuildConfig {
-    public static final boolean DEBUG = true;
-
-    public static final int VERSION_CODE = 1;
-
     public static final String APPLICATION_ID = "com.basecamp.turbolinks";
-    public static final String BUILD_TYPE = "debug";
-    public static final String FLAVOR = "";
-    public static final String VERSION_NAME = "1.0";
+    public static final String BUILD_TYPE = BuildConfig.BUILD_TYPE;
+    public static final boolean DEBUG = BuildConfig.DEBUG;
+    public static final String FLAVOR = BuildConfig.FLAVOR;
+    public static final String VERSION_NAME = BuildConfig.VERSION_NAME;
+    public static final int VERSION_CODE = BuildConfig.VERSION_CODE;
 }
