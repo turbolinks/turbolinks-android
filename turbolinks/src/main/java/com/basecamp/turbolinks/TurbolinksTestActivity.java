7fc1691ad690680f057bca07a32e570d24079e24
Dan Kim <dan@basecamp.com>
2016-08-15 12:23:05 -0500

    Suppress lint check for instantiatable for dummy test class

--- turbolinks/src/main/java/com/basecamp/turbolinks/TurbolinksTestActivity.java
+++ turbolinks/src/main/java/com/basecamp/turbolinks/TurbolinksTestActivity.java
@@ -1,8 +1,10 @@
 package com.basecamp.turbolinks;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 
 /**
  * For testing purposes only. Robolectric sometimes struggles with mocks.
  */
+@SuppressLint("Instantiatable")
 class TurbolinksTestActivity extends Activity {}
